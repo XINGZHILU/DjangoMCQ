9141aba49ecde1ef23b1d9ef2b04ecc74657e0ed
XINGZHILU <96632421+XINGZHILU@users.noreply.github.com>
2024-06-09 00:50:25 +0100

    Update wsgi.py

--- DjangoMCQ/wsgi.py
+++ DjangoMCQ/wsgi.py
@@ -11,8 +11,13 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+import sys
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoMCQ.settings')
 
 application = get_wsgi_application()
 
 app = application
+
+if os.environ.get('ENV') == 'production':
+    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
