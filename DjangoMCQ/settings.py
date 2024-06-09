ab18217edb6fadca43d040aa5c6910a8ca46dea9
XINGZHILU <xingzhi.lu@outlook.com>
2024-06-09 17:32:24 +0100

    Postgresql version

--- DjangoMCQ/settings.py
+++ DjangoMCQ/settings.py
@@ -86,19 +86,21 @@ DATABASES = {
 }
 
 
-
 """
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'xingzhi',
+        'USER': 'postgres.mpnccuuftiwdzanumwvf',
         'PASSWORD': 'Legogeek23',
-        'HOST': 'ciemcq-db.postgres.database.azure.com',
-        'PORT': '5432',
+        'HOST': 'aws-0-eu-central-1.pooler.supabase.com',
+        'PORT': '6543',
     }
 }
 """
+
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
@@ -132,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'files'))
+STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'files'))]
 STATIC_ROOT = BASE_DIR / 'files'
 
 
