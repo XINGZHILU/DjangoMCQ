9651cbca04ab297f2957ebf084b5478ec268af48
XINGZHILU <xingzhi.lu@outlook.com>
2024-06-09 11:41:32 +0100

    Removed path

--- DjangoMCQ/settings.py
+++ DjangoMCQ/settings.py
@@ -72,7 +72,7 @@ WSGI_APPLICATION = 'DjangoMCQ.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -90,7 +90,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
