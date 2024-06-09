5e96043d11fd31dccbe032819c87c2e2df0a0b25
XINGZHILU <xingzhi.lu@outlook.com>
2024-06-09 10:19:30 +0100

    Removed path

--- DjangoMCQ/wsgi.py
+++ DjangoMCQ/wsgi.py
@@ -18,6 +18,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DjangoMCQ.settings')
 application = get_wsgi_application()
 
 app = application
-
-if os.environ.get('ENV') == 'production':
-    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
